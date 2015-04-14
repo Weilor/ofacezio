3860918f6d94c2e364a35fdf0b2f18d577128bd3
Acezio <bolide2005@163.com>
2015-04-14 22:54:19 +0800

    first client commit

---
+++ python project/data_type.py
@@ -0,0 +1,23 @@
+__author__ = '绍文'
+
+bag = ["hello", 78, 1.56, 2+3j]
+print(bag[2:3])
+print(bag * 2)
+
+dictionary = {}
+dictionary['first name'] = "han"
+dictionary['last name'] = 'shaowen'
+字典 = {"姓名": "韩绍文", "年龄": 24}
+print("---------------------------------\n")
+print(dictionary)
+print(dictionary['first name'])
+print(字典["年龄"])
+print(dictionary.keys())
+print(字典.values())
+
+floatnum = 0xe
+interger = int(floatnum)
+print("--------------------------------\n")
+print(interger)
+
+
