609430fd636f7a8417b806c7cf757a4e919e6323
Acezio <bolide2005@163.com>
2015-04-22 21:22:19 +0800

    000 给图片增加右上方数字

--- python project/pil_drawreddot.py
+++ python project/pil_drawreddot.py
@@ -2,13 +2,13 @@ __author__ = '绍文'
 
 from PIL import Image, ImageDraw, ImageFont
 
-im = Image.open("paul.jpg")
+im = Image.open("005.jpg")
 draw = ImageDraw.Draw(im)
 w_width, h_height = im.size
 text = "19"
-font = ImageFont.truetype("STXIHEI.TTF", 25)
+font = ImageFont.truetype("STXIHEI.TTF", 300)
 w_font, h_font = draw.textsize(text,font)
-draw.ellipse(((w_width - w_font, 3), (w_width, w_font + 3)), fill="#D00000", outline=None)
+draw.ellipse(((w_width - w_font, 30), (w_width, w_font + 30)), fill="#D00000", outline=None)
 draw.text((w_width - w_font, 0), text, fill="white", font=font)
 size = (128, 128)
 im.thumbnail(size)
