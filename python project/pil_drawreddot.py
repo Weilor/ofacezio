e8ce5925d2e3321fe2ae4623a9dd613e79d0f573
Acezio <bolide2005@163.com>
2015-04-22 22:18:01 +0800

    000 给头像右上角加红点 Final

--- python project/pil_drawreddot.py
+++ python project/pil_drawreddot.py
@@ -4,14 +4,10 @@ from PIL import Image, ImageDraw, ImageFont
 
 im = Image.open("007.jpg")
 w_width, h_height = im.size
-back_size = (w_width + 20, h_height + 20)
-im_back = Image.new("RGBA", back_size, color=(255, 255, 255))
-b_width, b_height = im_back.size
-im_back.paste(im, (10, 10))
-draw = ImageDraw.Draw(im_back)
+draw = ImageDraw.Draw(im)
 text = "19"
 font = ImageFont.truetype("STXIHEI.TTF", 30)
 w_font, h_font = draw.textsize(text,font)
-draw.ellipse(((b_width - w_font, 3), (b_width, w_font + 3)), fill="#D00000", outline=None)
-draw.text((b_width - w_font, 0), text, fill="white", font=font)
-im_back.save("reddot.jpg")
+draw.ellipse(((w_width - w_font, 3), (w_width, w_font + 3)), fill="#D00000", outline=None)
+draw.text((w_width - w_font, 0), text, fill="white", font=font)
+im.save("reddot.jpg")
