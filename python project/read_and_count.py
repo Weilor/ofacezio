66af80021712770a9fa775fb24c841f76f7beca1
Acezio <bolide2005@163.com>
2015-05-03 10:03:19 +0800

    重新学习正则表达式和file IO

--- python project/read_and_count.py
+++ python project/read_and_count.py
@@ -3,15 +3,8 @@ from string import punctuation
 import re
 
 fp = open("zenofpython.txt", "r")       # open text file
-string_list = fp.readlines()            # read all line from file
-word_list = {}
-for string in string_list:
-    word = string.split(" ")            # slit lines whit space, then i can get all single word
-    for word_single in word:
-        wd = re.sub(r'[{}]+'.format(punctuation), '', word_single.strip())   # remove punctuation from word
-        if wd in word_list.keys():
-            word_list[wd] += 1          # count every word
-        else:
-            word_list[wd] = 1
-for k, v in word_list.items():
-    print(k, "count", v)
+string = fp.read()
+word = re.findall(r'[a-zA-z0-9]+', string)
+
+if __name__ == "__main__":
+    print(len(word))
